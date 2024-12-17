812733ab475749cfce767851f63d319871bb151a
Ilyusha-zzz <conorsalex160@gmail.com>
2024-12-17 13:19:59 +0300

    first commit

---
+++ leson_async/n_goroutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func n_Goroutine() {
+	r := rand.New(rand.NewSource((time.Now().Unix())))
+	var wg sync.WaitGroup
+	maxVar := r.Intn(100)
+
+	wg.Add(maxVar)
+	for i := 0; i < maxVar; i++ {
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf("i: %v\n", i)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Printf("maxVar: %v\n", maxVar)
+}
